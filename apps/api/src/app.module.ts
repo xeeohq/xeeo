c09d9612617224afc6c7e9937e0c8f22eb2ada6a
suraj-86 <harshsuraj8676@gmail.com>
2026-07-16 15:04:56 +0530

    feat(api): integrate Prisma with PostgreSQL

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,18 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { envValidationSchema } from './config/env.validation';
-import appConfig from './config/app.config';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { envValidationSchema } from "./config/env.validation";
+import appConfig from "./config/app.config";
+import { PrismaModule } from "./prisma";
 
 @Module({
   imports: [
-  ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: '.env',
-    validationSchema: envValidationSchema,
-    load: [appConfig],  
-  }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+      validationSchema: envValidationSchema,
+      load: [appConfig],
+    }),
+    PrismaModule,
   ],
   controllers: [],
   providers: [],
---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,32 @@
+import {
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    super({
+      adapter: new PrismaPg(pool),
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
